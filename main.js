6eed0ce1c00135fdca56b589a7d858467abec646
Sh4ke96 <Sh4ke96@gmail.com>
2021-06-30 17:22:05 +0200

    add hidden output

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const btn = document.getElementById("btn");
+const p = document.querySelectorAll(".output-p");
+
 let dollars = document.getElementById("output1");
 let yen = document.getElementById("output2");
 let euro = document.getElementById("output3");
@@ -9,18 +11,22 @@ let formatter1 = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
 });
+
 let formatter2 = new Intl.NumberFormat("ja-JP", {
   style: "currency",
   currency: "JPY",
 });
+
 let formatter3 = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "EUR",
 });
+
 let formatter4 = new Intl.NumberFormat("en-GB", {
   style: "currency",
   currency: "GBP",
 });
+
 let formatter5 = new Intl.NumberFormat("en-IN", {
   style: "currency",
   currency: "INR",
@@ -33,4 +39,5 @@ btn.addEventListener("click", () => {
   euro.innerHTML = `<span>Euro:</span> ${formatter3.format(amount)}`;
   pound.innerHTML = `<span>British Pound:</span> ${formatter4.format(amount)}`;
   rupees.innerHTML = `<span>Indian Rupees:</span> ${formatter5.format(amount)}`;
+  p.forEach((p) => (p.style.display = "block"));
 });
